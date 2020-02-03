5ef330de1a9adb38d8f5ee36519dcf6ee79b300b
Negin <nuggetnegin@gmail.com>
2020-02-03 10:24:52 -0500

    pseudo code logic

---
+++ etchasketch.js
@@ -0,0 +1,15 @@
+console.log('it works')
+
+/*select elements ont he page:
+canvas, shake button*/
+
+
+/*setup canvas for drawing*/
+
+/*write a draw function*/
+
+/*event handler for the keys*/
+
+/*clear or shake function*/
+
+/*listen for arrow keys*/
