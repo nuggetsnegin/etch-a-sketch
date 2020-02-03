d19f5a51fbeb2f7534706f67080e3b1c5dd1dd74
Negin <nuggetnegin@gmail.com>
2020-02-03 10:42:40 -0500

    set up basic etch-a-sketch properties and got a dot on the page!

--- etchasketch.js
+++ etchasketch.js
@@ -4,10 +4,19 @@ console.log('it works')
 canvas, shake button*/
 const canvas = document.querySelector('#etch-a-sketch'); /*id tag*/
 const ctx = canvas.getContext('2d');  /*setting context of the canvas to 2d*/
+console.log(ctx);
 const shakeButton = document.querySelector('.shake'); /*grabbing button*/
 
-
 /*setup canvas for drawing*/
+ctx.lineJoin = 'round'; /*set the *brush* it so it's rounded*/
+ctx.lineCap = 'round';
+ctx.lineWidth = 10; /*increased default width*/
+
+/*etch-a-sketch requires a dot on the page to start*/
+ctx.beginPath(); /*start the drawing*/
+ctx.moveTo(200, 200); /*where we're starting*/
+ctx.lineTo(200, 200); /*where we draw our line to*/
+ctx.stroke();
 
 /*write a draw function*/
 
