b5a9f420f4d24595f143ba5195aee3e790c1a1ef
Negin <nuggetnegin@gmail.com>
2020-02-03 10:45:07 -0500

    grabbing height and width of canvas so we can randomize starting point

--- etchasketch.js
+++ etchasketch.js
@@ -7,16 +7,30 @@ const ctx = canvas.getContext('2d');  /*setting context of the canvas to 2d*/
 console.log(ctx);
 const shakeButton = document.querySelector('.shake'); /*grabbing button*/
 
+/*need to actual width and height to randomize*/
+// const width = canvas.width;
+// const height = canvas.height;
+
+/*we can short form this using destructuring because we're using the same properties:*/
+const { width, height } = canvas;
+
+
 /*setup canvas for drawing*/
 ctx.lineJoin = 'round'; /*set the *brush* it so it's rounded*/
 ctx.lineCap = 'round';
 ctx.lineWidth = 10; /*increased default width*/
+
 
 /*etch-a-sketch requires a dot on the page to start*/
 ctx.beginPath(); /*start the drawing*/
 ctx.moveTo(200, 200); /*where we're starting*/
 ctx.lineTo(200, 200); /*where we draw our line to*/
 ctx.stroke();
+
+/*randomize the starting spot!*/
+
+
+
 
 /*write a draw function*/
 
